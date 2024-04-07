4d3159e799d17ae97c2ffd6683b2691fc79b210f
s27030 <s27030@pjwstk.edu.pl>
2024-04-07 19:44:10 +0200

    testUser added

---
+++ LegacyApp/TestUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LegacyApp;
+
+public class TestUser : User
+{
+    public TestUser(int creditLimit,bool hasCredit)
+    {
+        base.HasCreditLimit = hasCredit;
+        base.CreditLimit = CreditLimit;
+    }
+}
